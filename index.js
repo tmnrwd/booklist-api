6872c119020e102274e0a118eb4083a7ef18a7c0
tmnrwd <61465712+tmnrwd@users.noreply.github.com>
2020-09-15 14:53:20 +0100

    heroku deploy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')      //alternative to using import...
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000 || 80 || 443
 const router = require('./router')
 
 app.use(express.json());
